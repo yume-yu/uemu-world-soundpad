9b9f8f5362122aca6421659968983a487df8ad58
yume_yu <yu.matchang@gmail.com>
2021-11-04 00:50:38 +0900

    update script

---
+++ script.js
@@ -0,0 +1,15 @@
+(function (window, $) {
+  'use strict';
+
+  $.fn.useSound = function (_event, _id) {
+    var se = $(_id);
+    this.on(_event, function(){
+      se[0].currentTime = 0;
+      se[0].play();
+    });
+    return this;
+  };
+
+})(this, this.jQuery);
+
+$('.sound').useSound('mousedown touchstart', '#sound');
